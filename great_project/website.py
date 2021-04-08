1222eb328e7cbabd67238674074aa727fdbcb5d5
Adam Roe <adam.j.roe@gmail.com>
2021-04-08 11:49:50 +0200

    add environment configuration

--- great_project/website.py
+++ great_project/website.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask import render_template
 
-
 app = Flask(__name__)
+
+# configure Flask
+app.config.from_pyfile("config.py")
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', page_title="My great website")
+    return render_template('index.html',
+                           page_title=app.config["DATABASE_PASSWORD"])
+#    return render_template('index.html', page_title="My great website")
 
 
 if __name__ == "__main__":
